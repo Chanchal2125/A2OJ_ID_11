8869e579ead7ea6aeb00cccc31a825bb7424dbd3
Chanchal Gupta <61290731+Chanchal2125@users.noreply.github.com>
2020-12-07 16:10:28 +0530

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <climits>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+#define ll             long long int
+#define ld             long double
+#define mod            1000000007
+#define endl           "\n"
+#define vi             vector<ll>
+#define vs             vector<string>
+#define pii            pair<ll, ll>
+#define ump            unordered_map
+#define mp             map
+#define pq_max         priority_queue<ll>
+#define pq_min         priority_queue<ll,vi,greater<ll>
+#define ff             first
+#define ss             second
+#define mid(l,r)       (l+(r-l)/2)
+#define loop(i,a,b)    for(int i=(a); i <=(b);i++)
+#define looprev(i,a,b) for(int i=(a); i >=(b);i--)
+#define clr(val)       memset(val,0,sizeof(val))
+#define what_is(x)     cerr << #x << " is " << x << endl; 
+#define OJ             \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+ 
+int main() 
+{
+    string s;
+    cin>>s;
+ 
+    loop(i,0,s.length()-1){
+        if(s[i] == '.') cout<<"0";
+        else if (s[i] == '-' && s[i+1] == '.'){
+            cout<<"1";
+            i++;
+        }
+        else if (s[i] == '-' && s[i+1] == '-'){
+            cout<<"2";
+            i++;
+        }
+    }
+
+return 0;
+   
+}
