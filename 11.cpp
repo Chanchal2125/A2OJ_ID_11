5cff04708fc583fe09e8384a9296e3c86a90140e
Chanchal Gupta <61290731+Chanchal2125@users.noreply.github.com>
2020-12-08 20:07:15 +0530

    Add files via upload

---
+++ 11.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <climits>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+#define ll             long long int
+#define ld             long double
+#define mod            1000000007
+#define endl           "\n"
+#define vi             vector<ll>
+#define vs             vector<string>
+#define pii            pair<ll, ll>
+#define ump            unordered_map
+#define mp             map
+#define pq_max         priority_queue<ll>
+#define pq_min         priority_queue<ll,vi,greater<ll>
+#define ff             first
+#define ss             second
+#define mid(l,r)       (l+(r-l)/2)
+#define loop(i,a,b)    for(int i=(a); i <=(b);i++)
+#define looprev(i,a,b) for(int i=(a); i >=(b);i--)
+#define clr(val)       memset(val,0,sizeof(val))
+#define what_is(x)     cerr << #x << " is " << x << endl; 
+#define OJ             \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+ 
+int a, b, h = 0;
+int mas[16] = {
+        2,
+        3,
+        5,
+        7,
+        11,
+        13,
+        17,
+        19,
+        23,
+        29,
+        31,
+        37,
+        41,
+        43,
+        47
+};
+int main() {
+    cin >> a >> b;
+    for (int i = 0; i < 15; i++) {
+        if (a == mas[i] && b == mas[i + 1]) {
+            h++;
+            break;
+        }
+    }
+    if (h == 0) {
+        cout << "NO";
+    } else {
+        cout << "YES";
+    }
+}
