07c209ef5588e4ae1fc0e08ee8a4be36ba70a39f
Chanchal Gupta <61290731+Chanchal2125@users.noreply.github.com>
2020-12-08 20:10:19 +0530

    Add files via upload

---
+++ 20.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <climits>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+#define ll             long long int
+#define ld             long double
+#define mod            1000000007
+#define endl           "\n"
+#define vi             vector<ll>
+#define vs             vector<string>
+#define pii            pair<ll, ll>
+#define ump            unordered_map
+#define mp             map
+#define pq_max         priority_queue<ll>
+#define pq_min         priority_queue<ll,vi,greater<ll>
+#define ff             first
+#define ss             second
+#define mid(l,r)       (l+(r-l)/2)
+#define loop(i,a,b)    for(int i=(a); i <=(b);i++)
+#define looprev(i,a,b) for(int i=(a); i >=(b);i--)
+#define clr(val)       memset(val,0,sizeof(val))
+#define what_is(x)     cerr << #x << " is " << x << endl; 
+#define OJ             \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+ 
+int main() 
+{
+    string s;
+    cin>>s;
+
+    for(ll i=0;i<s.length();i+=2){
+        for(ll j=0;j<s.length()-i-2;j+=2){
+            if(s[j]>s[j+2]){
+                swap(s[j],s[j+2]);
+            }
+        }
+    }
+ 
+    cout<<s;
+ 
+return 0;
+   
+}
